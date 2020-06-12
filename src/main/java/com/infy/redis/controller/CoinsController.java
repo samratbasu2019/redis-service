a3f92018a50bd893a4e9dbbc951a3067a007cf25
Samrat <samtina.basu@gmail.com>
2020-06-12 22:41:17 +0530

    updated

--- src/main/java/com/infy/redis/controller/CoinsController.java
+++ src/main/java/com/infy/redis/controller/CoinsController.java
@@ -9,11 +9,13 @@ import org.apache.commons.logging.LogFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class CoinsController {
